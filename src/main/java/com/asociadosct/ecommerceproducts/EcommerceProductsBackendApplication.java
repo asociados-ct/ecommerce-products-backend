d6bb3bf112a5900e2318a065f7f2f34557b27bfc
jaimetellezb <jaimetellez410@gmail.com>
2020-04-25 20:19:31 -0500

    [INIT -jaimetellez ] -  Creación del proyecto backend en spring boot para aplicación de catálogo de productos

---
+++ src/main/java/com/asociadosct/ecommerceproducts/EcommerceProductsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.asociadosct.ecommerceproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProductsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asociadosct/ecommerceproducts/EcommerceProductsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asociadosct.ecommerceproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
