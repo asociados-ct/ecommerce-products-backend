361c12a006eb106a4ce58362f18d5d0b5ac9e204
jaimetellezb <jaimetellez410@gmail.com>
2020-06-29 16:11:16 -0500

    se actualiza las rutas CORS

--- src/main/java/com/asociadosct/ecommerceproducts/controller/UserController.java
+++ src/main/java/com/asociadosct/ecommerceproducts/controller/UserController.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://ecommerce-products-frontend.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/users")
