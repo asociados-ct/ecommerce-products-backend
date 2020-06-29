aa0ec8e3d4522f9f6d9f8b66b407ed694dbff174
Libardo Caceres <libardo.caceres@gmail.com>
2020-06-29 16:08:34 -0500

    Se realiza eliminación de la confinfiguración de los controller

--- src/main/java/com/asociadosct/ecommerceproducts/controller/UserController.java
+++ src/main/java/com/asociadosct/ecommerceproducts/controller/UserController.java
@@ -14,8 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://ecommerce-products-frontend.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
