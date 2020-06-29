03385195f74ffaa1d994e39509bf80bb07a6bc84
Libardo Caceres <libardo.caceres@gmail.com>
2020-06-29 07:08:24 -0500

    Modificacion de controller
    Se realiza modificacion de controller, para que se puedan consumer desde el frontend los servicios del backend

--- src/main/java/com/asociadosct/ecommerceproducts/controller/ProductController.java
+++ src/main/java/com/asociadosct/ecommerceproducts/controller/ProductController.java
@@ -13,7 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://ecommerce-products-frontend.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
